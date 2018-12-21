a1b8d971d0c0cab9f02894e864ea740992c23934
博 黄 <huangbo19971109@outlook.com>
2018-12-21 14:54:22 +0800

    创建测试

---
+++ Rtree/Rtree/base.h
@@ -0,0 +1,6 @@
+#pragma once
+#include<bits/stdc++.h>
+
+using std::cin;
+using std::cout;
+using std::endl;
---
+++ Rtree/Rtree/main.cpp
@@ -0,0 +1,6 @@
+#include "base.h"
+
+int main() {
+	cout << "Test" << endl;
+	return 0;
+}
